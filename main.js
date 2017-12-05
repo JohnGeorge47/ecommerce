1c2b03ec4ed834045fb6a58439d8351e85d46859
John <john.george@mountblue.io>
2017-12-05 06:06:55 +0530

    Add the product page

---
+++ main.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $('#back').click(function(){
+        console.log('djsajda');
+
+        $('#pic').attr('src','images/back.jpg')
+
+
+
+    });
+
+    $('#front').click(function(){
+        console.log('djsajda');
+
+        $('#pic').attr('src','images/dress2.jpg')
+
+
+
+    });
+
+});
